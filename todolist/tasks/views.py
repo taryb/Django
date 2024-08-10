60582499922ccf0f61480ad85ad231fb5c5b5e02
tbo <tbounavong@gmail.com>
2024-08-10 13:23:09 -0700

    push todolist
    push

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/index.html', context)
